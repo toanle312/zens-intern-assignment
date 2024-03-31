83d4427f0bf20c353e30cb839a88e94a034c0daf
toanle312 <toanle3122002@gmail.com>
2024-03-31 15:52:05 +0700

    (algorithm): solve Mini-Max Sum

---
+++ algorithm/mini-max-sum.js
@@ -0,0 +1,27 @@
+const solution = (input) => {
+  const numbers = input.split(' ').map((x) => +x);
+
+  let min = (max = numbers.shift());
+
+  let min_sum = (max_sum = 0);
+
+  for (let number of numbers) {
+    if (number < min) {
+      max_sum += min;
+      min = number;
+    } else {
+      max_sum += number;
+    }
+
+    if (number > max) {
+      min_sum += max;
+      max = number;
+    } else {
+      min_sum += number;
+    }
+  }
+
+  console.log(min_sum, max_sum);
+};
+
+solution('8 3 5 7 9');
