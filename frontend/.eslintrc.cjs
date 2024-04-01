613e110840843492c766f1ce52d9236e15874936
toanle312 <toanle3122002@gmail.com>
2024-04-01 14:15:11 +0700

    (frontend): config eslint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -12,10 +12,25 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
+    // React
+    'react/prop-types': 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'no-extra-boolean-cast': 0,
+    'no-lonely-if': 1,
+    'no-unused-vars': 1,
+    'no-trailing-spaces': 1,
+    'no-multi-spaces': 1,
+    'no-multiple-empty-lines': 1,
+    'space-before-blocks': ['error', 'always'],
+    'object-curly-spacing': [1, 'always'],
+    'array-bracket-spacing': 1,
+    'linebreak-style': 0,
+    'no-unexpected-multiline': 'warn',
+    'keyword-spacing': 1,
+    'comma-spacing': 1,
+    'arrow-spacing': 1,
   },
 };
